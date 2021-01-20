d87394140eb84e6cc56d4df1e13c2a38b2c6e975
nikkaushal <nikkaushal@gmail>
2021-01-19 21:01:02 -0500

    asg

--- asg.tf
+++ asg.tf
@@ -40,9 +40,15 @@ resource "aws_security_group" "allow-component" {
     from_port               = 22
     to_port                 = 22
     protocol                = "tcp"
-    cidr_blocks             = ["0.0.0.0/0"]
+    cidr_blocks             = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
-
+  ingress {
+    description             = "HTTP"
+    from_port               = 80
+    to_port                 = 80
+    protocol                = "tcp"
+    cidr_blocks             = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
   egress {
     from_port              = 0
     to_port                = 0
